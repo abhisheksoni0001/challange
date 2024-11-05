6bc29949b76c3e1ec79bbf1f9dd9547e8b4502db
Abhishek soni <131152905+abhisheksoni0001@users.noreply.github.com>
2024-11-05 17:14:48 +0530

    Update test_pandas_version.py

--- test_pandas_version.py
+++ test_pandas_version.py
@@ -2,15 +2,16 @@
 Test for correct pandas version
 '''
 import pandas as pd
-
+import numpy as np
 
 PANDAS_VERSION = "1.5.3"
-
+numpy = "1.23.5"
 
 def test_pandas_version():
     ''' Use an assertion to check the output of pd.__version__ '''
     assert pd.__version__ in [PANDAS_VERSION]
-
+    assert np.__version__ in [numpy]
+    
 
 if __name__ == "__main__":
     test_pandas_version()
